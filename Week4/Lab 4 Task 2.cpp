5bbb9da56806bb1ddd8a9f73498c3da5605a0e83
hanangillani543216-coder <hanangillani543216@gmail.com>
2025-11-30 21:00:59 +0500

    Update Lab 4 Task 2.cpp

--- Week4/Lab 4 Task 2.cpp
+++ Week4/Lab 4 Task 2.cpp
@@ -6,5 +6,6 @@ int age;
 cout << "Enter age: ";
 cin >> age;
 (age < 13) ? cout << "Age " << age << " falls under the category Child." : (age >= 13 && age <=19) ? cout << "Age " << age << " falls under the category Teeenager." : cout << "Age " << age << " falls under the category Adult.";
+(age < 13) ? cout << "Age " << age << " falls under the category Child." : (age >= 13 && age <=19) ? cout << "Age " << age << " falls under the category Teenager." : cout << "Age " << age << " falls under the category Adult.";
 return 0;
 }
